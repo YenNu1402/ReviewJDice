6c5cfd39bbd8c5a8129ae0d1ae1f0503d92ed168
Long0402 <Hoalong911920@gmail.com>
2025-04-24 14:31:36 +0700

    Validate

--- JDice.java
+++ JDice.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,24 +7,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 /*
 JDice: Java Dice Rolling Program
-Copyright (C) 2006 Andrew D. Hilton  (adhilton@cis.upenn.edu)
+Copyright (C) 2006 Andrew D. Hilton
 
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+This program is free software...
 
  */
+
 public class JDice {
     public static final Logger LOGGER = Logger.getLogger(JDice.class.getName());
     static final String CLEAR = "Clear";
@@ -33,47 +20,43 @@ public class JDice {
 
     static void showError(String s) {
         JOptionPane.showConfirmDialog(null, s, "Error", JOptionPane.ERROR_MESSAGE);
+        LOGGER.log(Level.WARNING, "Error displayed: " + s); // Ghi log khi có lỗi
     }
 
     private static class JDiceListener implements ActionListener {
-
         Vector<String> listItems;
         JList<String> resultList;
         JComboBox<String> inputBox;
         long lastEvent;
 
-        /* hack to prevent double events with text
-			   entry */
-        public JDiceListener(JList<String> resultList,
-                JComboBox<String> inputBox) {
-
-            this.listItems = new Vector<String>();
+        public JDiceListener(JList<String> resultList, JComboBox<String> inputBox) {
+            this.listItems = new Vector<>();
             this.resultList = resultList;
             this.inputBox = inputBox;
             lastEvent = 0;
         }
 
         public void actionPerformed(ActionEvent e) {
-
-            if (e.getWhen() == lastEvent) {
-                return;
-            }
+            if (e.getWhen() == lastEvent) return;
             lastEvent = e.getWhen();
-            if (e.getSource() instanceof JComboBox
-                    || e.getActionCommand().equals(ROLL)) {
+
+            if (e.getSource() instanceof JComboBox || e.getActionCommand().equals(ROLL)) {
                 String s = inputBox.getSelectedItem().toString();
                 String[] arr = s.split("=");
                 String name = "";
-                for (int i = 0; i < arr.length - 2; i++) {+
-                    name = arr[i] + "=";
+                for (int i = 0; i < arr.length - 2; i++) {
+                    name += arr[i] + "=";
                 }
                 if (arr.length >= 2) {
-                    name = name + arr[arr.length - 2];
+                    name += arr[arr.length - 2];
                 }
+                LOGGER.log(Level.INFO, "Rolling from combo input: " + s); // Ghi log hành động ROLL
                 doRoll(name, arr[arr.length - 1]);
             } else if (e.getActionCommand().equals(CLEAR)) {
+                LOGGER.log(Level.INFO, "User clicked CLEAR"); // Ghi log khi Clear
                 doClear();
             } else {
+                LOGGER.log(Level.INFO, "Rolling via button: " + e.getActionCommand()); // Log nút Roll riêng
                 doRoll(null, e.getActionCommand());
             }
         }
@@ -84,85 +67,83 @@ public class JDice {
             resultList.setListData(listItems);
         }
 
-        private void doRoll(String name,
-                String diceString) {
+        private void doRoll(String name, String diceString) {
             String prepend = "";
             int start = 0;
-            int i;
             Vector<DieRoll> v = DiceParser.parseRoll(diceString);
             if (v == null) {
-                showError("Invalid dice string" + diceString);
+                showError("Invalid dice string: " + diceString);
                 return;
             }
+
             if (name != null) {
                 listItems.add(0, name);
                 start = 1;
                 prepend = "  ";
             }
+
             int[] selectionIndices = new int[start + v.size()];
-            for (i = 0; i < v.size(); i++) {
+			for (int i = 0; i < v.size(); i++) {
                 DieRoll dr = v.get(i);
                 RollResult rr = dr.makeRoll();
                 String toAdd = prepend + dr + "  =>  " + rr;
                 listItems.add(i + start, toAdd);
             }
-            for (i = 0; i < selectionIndices.length; i++) {
+
+            for (int i = 0; i < selectionIndices.length; i++) {
                 selectionIndices[i] = i;
             }
+
             resultList.setListData(listItems);
             resultList.setSelectedIndices(selectionIndices);
         }
-
     }
 
     public static void main(String[] args) {
-        Vector<String> v = new Vector<String>();
+        Vector<String> v = new Vector<>();
         if (args.length >= 1) {
             try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-
                 String s;
                 while ((s = br.readLine()) != null) {
                     v.add(s);
                 }
             } catch (IOException ioe) {
                 ioe.printStackTrace();
-                LOGGER.log(Level.SEVERE,"***********\n**********\n", ioe);
-                LOGGER.log(Level.SEVERE,"Could not read input file: " + args[0]);
-                LOGGER.log(Level.SEVERE,"***********\n**********\n");
+                LOGGER.log(Level.SEVERE, "Could not read input file: " + args[0], ioe);
             }
-
         }
+
         JFrame jf = new JFrame("Dice Roller");
         Container c = jf.getContentPane();
         c.setLayout(new BorderLayout());
+
         JList<String> jl = new JList<>();
-        c.add(jl, BorderLayout.CENTER);
+        JScrollPane scrollPane = new JScrollPane(jl); // Bọc JList trong JScrollPane
+        c.add(scrollPane, BorderLayout.CENTER);       // Giúp hiển thị đẹp khi nhiều dòng
+
         JComboBox<String> jcb = new JComboBox<>(v);
         jcb.setEditable(true);
         c.add(jcb, BorderLayout.NORTH);
+
         JDiceListener jdl = new JDiceListener(jl, jcb);
         jcb.addActionListener(jdl);
+
         JPanel rightSide = new JPanel();
-        rightSide.setLayout(new BoxLayout(rightSide,
-                BoxLayout.Y_AXIS));
-        String[] buttons = {ROLL,
-            "d4",
-            "d6",
-            "d8",
-            "d10",
-            "d12",
-            "d20",
-            "d100",
-            CLEAR};
-        for (int i = 0; i < buttons.length; i++) {
-            JButton newButton = new JButton(buttons[i]);
+        rightSide.setLayout(new BoxLayout(rightSide, BoxLayout.Y_AXIS));
+        String[] buttons = {ROLL, "d4", "d6", "d8", "d10", "d12", "d20", "d100", CLEAR};
+
+        for (String button : buttons) {
+            JButton newButton = new JButton(button);
             rightSide.add(newButton);
             newButton.addActionListener(jdl);
         }
+
         c.add(rightSide, BorderLayout.EAST);
+
         jf.setSize(450, 500);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);
 
+        LOGGER.log(Level.INFO, "JDice GUI started."); // Log khi chương trình khởi động
     }
 }
