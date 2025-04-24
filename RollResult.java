dc4c836e495cc4e97db0b1785423b3ac1d5f329d
ThanhHang060605 <phamthanhhangqp66@gmai.com>
2025-04-24 14:31:22 +0700

    Refactor code

--- RollResult.java
+++ RollResult.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class RollResult {
@@ -18,26 +19,17 @@ public class RollResult {
         rolls = new Vector<>();
     }
 
-    // Thêm logging để theo dõi kết quả thêm vào
     public void addResult(int res) {
         total += res;
         rolls.add(res);
-        System.out.println("[LOG] Added roll: " + res + ", new total: " + total);
     }
 
-    // Refactor để dùng biến newTotal, tránh nhầm lẫn với this.total
     public RollResult andThen(RollResult r2) {
         int newTotal = this.total + r2.total;
-        int newModifier = this.modifier + r2.modifier;
-
-        Vector<Integer> combinedRolls = new Vector<>();
-        combinedRolls.addAll(this.rolls);
-        combinedRolls.addAll(r2.rolls);
-
-        System.out.println("[LOG] Merged rolls: " + combinedRolls);
-        System.out.println("[LOG] New total: " + newTotal + ", New modifier: " + newModifier);
-
-        return new RollResult(newTotal, newModifier, combinedRolls);
+        Vector<Integer> rolls = new Vector<>();
+        rolls.addAll(this.rolls);
+        rolls.addAll(r2.rolls);
+        return new RollResult(newTotal, this.modifier + r2.modifier,rolls);
     }
 
     public String toString() {
