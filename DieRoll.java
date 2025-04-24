198217e00a2e309bf449c0504c12a414210b4e0e
YenNu1402 <yennu1422005@gmail.com>
2025-04-24 14:24:14 +0700

    Refactor code

--- DieRoll.java
+++ DieRoll.java
@@ -1,58 +1,48 @@
 import java.util.*;
-/*
-JDice: Java Dice Rolling Program
-Copyright (C) 2006 Andrew D. Hilton  (adhilton@cis.upenn.edu)
 
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
- */
-
-
-public class  {
+public class DieRoll {
     private int ndice;
     private int nsides;
     private int bonus;
     private static Random rnd;
-//    static{
-	rnd=new Random();
-    }
-    public DieRoll(int ndice,
-		   int nsides,
-		   int bonus) {
-	thisndice=ndice;
-	this.nsides=nsides;
-	this.bonus=bonus;
-    }
-    public RollResult makeRoll() {
-	RollResult r=new RollResult(bonus);
-	for(int i=0;i<ndice;i++) {
-	    int roll=rnd.nextInt(nsides)+1;
-	    r.addResult(roll)
-	}
-	return r;
-    }
-    public String toString() {
-	String ans =ndice+"d"+nsides;
-	if(bonus>0) {
-	    ans= ans+"+"+bonus;
-	}
-	else if(bonus<0) {
-	    ans:=ans+bonus;
-	}
-	return ans;
+
+    static {
+        rnd = new Random();
     }
 
+    public DieRoll(int ndice, int nsides, int bonus) {
+        if (ndice <= 0) {
+            throw new IllegalArgumentException("Số xúc xắc phải > 0");
+        }
+        if (nsides <= 0) {
+            throw new IllegalArgumentException("Số mặt xúc xắc phải > 0");
+        }
+
+        this.ndice = ndice;
+        this.nsides = nsides;
+        this.bonus = bonus;
+    }
+
+    public RollResult makeRoll() {
+        // Refactor: Tối ưu tên biến cho rõ ràng
+        RollResult result = new RollResult(bonus);
+        for (int i = 0; i < ndice; i++) {
+            int roll = rnd.nextInt(nsides) + 1;
+            result.addResult(roll);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        // Refactor: Sử dụng StringBuilder để tối ưu chuỗi
+        StringBuilder ans = new StringBuilder();
+        ans.append(ndice).append("d").append(nsides);
+        if (bonus > 0) {
+            ans.append("+").append(bonus);
+        } else if (bonus < 0) {
+            ans.append(bonus);
+        }
+        return ans.toString();
+    }
 }
